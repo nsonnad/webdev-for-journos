88e86ad1ecaf564574e2e777657eab18e42d41d3
Nikhil S <nikhil@nsnnd.com>
2023-05-02 16:16:54 -0500

    week 11 stuff

--- examples/week-10/script.js
+++ examples/week-10/script.js
@@ -1,5 +1,5 @@
 // SVG+D3 margin convention
-const margin = {top: 20, right: 30, bottom: 40, left: 20},
+const margin = {top: 20, right: 30, bottom: 40, left: 100},
     width = 900 - margin.left - margin.right,
     height = 600 - margin.top - margin.bottom;
 
@@ -11,11 +11,81 @@ let svg = d3.select("#dataviz")
         .append("g")
             .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-// linearScale
+const someData = [100000, 250000, 600000, 800000, 950000]
 
-// timeScale
+const gunData = [
+    {
+        "Country": "United States",
+        "Value": 12394
+    },
+    {
+        "Country": "Russia",
+        "Value": 6148
+    },
+    {
+        "Country": "Germany (FRG)",
+        "Value": 1653
+    },
+    {
+        "Country": "France",
+        "Value": 2162
+    },
+    {
+        "Country": "United Kingdom",
+        "Value": 1214
+    },
+    {
+        "Country": "China",
+        "Value": 1131
+    },
+    {
+        "Country": "Spain",
+        "Value": 814
+    },
+    {
+        "Country": "Netherlands",
+        "Value": 1167
+    },
+    {
+        "Country": "Italy",
+        "Value": 660
+    },
+    {
+        "Country": "Israel",
+        "Value": 1263
+    }
+];
+
+// scaleLinear
+const xScale = d3.scaleLinear()
+  .domain([0, 15000]) // input data 
+  .range([0, width]) // visual display
+
+// const circleSizeScale = d3.scaleLinear()
+//   .domain([0, 1000000])
+//   .range([1, 25])
 
 // scaleBand
+const yScale = d3.scaleBand()
+  .domain(gunData.map(d => d.Country))
+  .range([height, 0])
+
+const colorScale = d3.scaleLinear()
+  .domain([2000, 15000])
+  .range(["orange", "darkred"])
 
 // use d3 to draw axes based on the scales
-// svg.append("g").call(d3.axisBottom(xScale))
+svg.append("g").call(d3.axisLeft(yScale))
+
+svg.append("g")
+    .attr("transform", `translate(0, ${height})`)
+    .call(d3.axisBottom(xScale))
+
+svg.selectAll("rect")
+   .data(gunData)
+   .join("rect")
+   .attr("x", xScale(0))
+   .attr("y", (d) => { return yScale(d.Country) })
+   .attr("width", (d) => { return xScale(d.Value) })
+   .attr("height", yScale.bandwidth() - 2)
+   .attr("fill", (d => colorScale(d.Value)))
---
+++ slides/markdown/week-11.md
@@ -0,0 +1,38 @@
+# Turning our ideas into code~
+
+---
+
+# Share your visions
+
+Let's go around the room:
+
+1. Show us your sketch!
+2. Tell: How are you making the important visible? What's the key point you want
+   to show with your visualization?
+3. What types of scales will you need to create?
+4. What do others think? Anything missing? Questions that come up?
+
+---
+
+# Today's goals
+
+1. Create a repo for your project
+2. Copy the boilerplate html and JavaScript code from last week to load D3 and set
+   up an SVG container with margins
+3. Write code for all the scales you'll need for your visualization (feel free
+   to copy code from last week)
+4. Use the scales you created to draw axes
+
+No actual data yet; just the foundation!
+
+---
+
+# For next week
+
+Get your dataset ready for visualization
+
+1. Do any data analysis, prep, cleanup, etc you need for your data to contain
+   just the information you want to show
+-> Keep in mind the principle of making the important visible: Is any of your data unnecessary? Should you be doing any simple calculations to emphasize a particular point?
+2. When it's ready, export your dataset to a CSV and add it to your
+   visualization repo
